daff6c58a5ba7d0f330ebf8330ecf53a3615cf0b
WebD00D <christian@brewrocket.io>
2015-11-24 14:36:07 -0500

    finalized stripe code

--- UniversalRoom/engine.asmx.vb
+++ UniversalRoom/engine.asmx.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
+Imports Stripe
 
 ' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
 <System.Web.Script.Services.ScriptService()> _
@@ -11,13 +12,24 @@ Public Class engine
     Inherits System.Web.Services.WebService
 
     <WebMethod()> _
-    Public Function makeDonation(ByVal token As String, ByVal amount As String, ByVal email As String) As String
+    Public Sub makeDonation(ByVal token As String, ByVal amount As String, ByVal email As String)
 
+        Dim AmountToCharge As Decimal = 0
+        If amount = "20" Then AmountToCharge = 2000
+        If amount = "50" Then AmountToCharge = 5000
+        If amount = "100" Then AmountToCharge = 10000
 
+        StripeConfiguration.SetApiKey("sk_test_6JBscnzT4PeUmkgYfLjGdS0p")
+        Dim myCharge = New StripeChargeCreateOptions()
+        myCharge.Amount = AmountToCharge
+        myCharge.Currency = "usd"
+        myCharge.Description = "Universal Room Donation"
+        myCharge.Source = New StripeSourceOptions() With {.TokenId = token}
+        myCharge.Capture = True
+        Dim chargeService = New StripeChargeService()
+        Dim stripeCharge As StripeCharge = chargeService.Create(myCharge)
 
-
-        Return "Hello World"
-    End Function
+    End Sub
 
     <WebMethod()> _
     Public Function HelloWorld() As String
